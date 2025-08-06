edb318d22a6b1eb690171b0aa73ad2146e845420
ryanshang <ryan.d.shang@gmail.com>
2025-08-06 12:05:12 -0400

    day1 homework

---
+++ homework/day1/data_types.py
@@ -0,0 +1,31 @@
+# int
+a = 12
+print("a", a, type(a))
+
+# float
+b = 1.346
+print("b", b, type(b))
+
+# bool
+c = True
+print("c", c, type(c))
+
+# str
+d = "Say hello!"
+print("d", d, type(d))
+
+# list
+e = [1, 3, 56, "omg", True, 1.231, "Thx all!", (1, 2, 4), {1231231, 2132}]
+print("e", e, type(e))
+
+# tuple
+f = (1, 3, "123", True, 3, 3, 3)
+print("f", f, type(f))
+
+# dict
+g = {1: 3, 2: 4}
+print("g", g, type(g))
+
+# set
+h = {1, 2, 3, 3, 3, 3}
+print("h", h, type(h))
---
+++ homework/day1/format_print.py
@@ -0,0 +1,4 @@
+username = input("Please type in your username: ")
+password = input("Please type in your password: ")
+
+print(f"Your username is: {username}, and your password is {password}.")
